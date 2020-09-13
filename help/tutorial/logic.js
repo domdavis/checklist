662f83a9067aee8964f8c2972b483b4c1521f645
Dom Davis <dom@domdavis.com>
2020-09-13 17:48:07 +0100

    Fix #20: Improve how we build the checks

--- help/tutorial/logic.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-  $('#blue').change(function() {
-    new Toggle('blue').colour('btn-primary')
-  })
-  $('#yellow').change(function() {
-    new Toggle('yellow').colour('btn-warning')
-  })
-})
---
+++ js/setup.js
@@ -0,0 +1,24 @@
+$(function () {
+  $('#fly').click(function() {
+    const aircraft = $('#aircraft').val()
+
+    if (!aircraft) {
+      error('aircraft', 'No aircraft selected!')
+      return
+    }
+
+    let params = {}
+
+    $('input').each((i, opt) => {
+      if (opt.type === 'checkbox') {
+        params[opt.id] = opt.checked
+      }
+
+      if (opt.type === 'text') {
+        params[opt.id] = opt.value
+      }
+    })
+
+    location.href = '/' + aircraft + "?" + $.param(params)
+  })
+})
